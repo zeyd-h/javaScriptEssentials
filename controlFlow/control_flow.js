79c49bfd61522d83ff4d1790e6484a072278a7b8
zeyd-h <z.hajhussien@gmail.cil.com>
2025-05-03 13:07:56 -0400

    message

--- controlFlow/control_flow.js
+++ controlFlow/control_flow.js
@@ -45,3 +45,7 @@ switch (userType) {
 
 console.log("User Category:", userCategory);
 
+let isAuthenticated = true;
+let authenticationStatus = isAuthenticated ? "Authenticated" : "Not authenticated";
+
+console.log("Authentication Status:", authenticationStatus);
